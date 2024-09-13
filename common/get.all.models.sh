dd3a29a13acd5beb658cc1fdd5d02e25d248c1e5
Jay Bee <56894914+jtabox@users.noreply.github.com>
2024-09-13 02:38:50 +0200

    added some models + nodes to download

--- common/get.all.models.sh
+++ common/get.all.models.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
 # This is run manually after the initial provisioning and downloads all the models specified below
 
-MODELS=(
-    "sdmodels/Checkpoints/Flux/flux1-dev-fp8.safetensors"
-    "sdmodels/VAE/flux-ae.safetensors"
+# For some reason the target folders don't get created during ini, so I do it here
+
+UNET_MODELS=(
+    "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors"
 )
-for model in "${MODELS[@]}"; do
-    fetch_url "$model"
+
+CLIP_MODELS=(
+    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors"
+    "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors"
+    "https://huggingface.co/zer0int/CLIP-GmP-ViT-L-14/resolve/main/ViT-L-14-BEST-smooth-GmP-TE-only-HF-format.safetensors"
+    "https://huggingface.co/zer0int/CLIP-GmP-ViT-L-14/resolve/main/ViT-L-14-TEXT-detail-improved-hiT-GmP-TE-only-HF.safetensors"
+)
+
+CKPT_MODELS=(
+    "https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors"
+    "https://civitai.com/api/download/models/727894?type=Model&format=SafeTensor&size=full&fp=fp8"
+)
+
+LORA_MODELS=(
+    "https://civitai.com/api/download/models/805890?type=Model&format=SafeTensor"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors"
+)
+
+SD_STORAGE_DIR="${WORKSPACE}/storage/stable_diffusion/models"
+
+mkdir -p "${SD_STORAGE_DIR}/unet"
+mkdir -p "${SD_STORAGE_DIR}/clip"
+mkdir -p "${SD_STORAGE_DIR}/ckpt"
+mkdir -p "${SD_STORAGE_DIR}/lora"
+mkdir -p "${SD_STORAGE_DIR}/vae"
+
+for model in "${UNET_MODELS[@]}"; do
+    fetch_url "$model" "${SD_STORAGE_DIR}/unet"
 done
 
-# will do it manually for now, need to source the mappings and the function
-# printf "\n:::::: Kustom Kloud Provisioner ::: Downloading basic models\n"
+for model in "${CLIP_MODELS[@]}"; do
+    fetch_url "$model" "${SD_STORAGE_DIR}/clip"
+done
 
-# rclone_download "ckpt" "sdmodels/Checkpoints/Flux/flux1-dev-fp8.safetensors"
-# rclone_download "ckpt" "sdmodels/Checkpoints/Flux/FluxBananadiffusion_v01NF4.safetensors"
-# rclone_download "clip" "sdmodels/CLIP/t5xxl_fp8_e4m3fn.safetensors"
-# rclone_download "clip" "sdmodels/CLIP/ViT-L-14-BEST-smooth-GmP-TE-only-HF-format.safetensors"
-# rclone_download "lora" "sdmodels/Lora/Flux/AndroFlux-v26.safetensors"
-# rclone_download "vae" "sdmodels/VAE/flux-ae.safetensors"
+for model in "${CKPT_MODELS[@]}"; do
+    fetch_url "$model" "${SD_STORAGE_DIR}/ckpt"
+done
+
+for model in "${LORA_MODELS[@]}"; do
+    fetch_url "$model" "${SD_STORAGE_DIR}/lora"
+done
+
+for model in "${VAE_MODELS[@]}"; do
+    fetch_url "$model" "${SD_STORAGE_DIR}/vae"
+done
+
+cecho green "::::: Finished downloading the models :::::"
--- common/get.all.nodes.sh
+++ common/get.all.nodes.sh
@@ -3,36 +3,36 @@
 # It uses https://github.com/ltdrdata/ComfyUI-Manager/blob/main/docs/en/cm-cli.md for complete install
 
 NODES=(
-    "https://github.com/11cafe/comfyui-workspace-manager"
-    "https://github.com/Acly/comfyui-inpaint-nodes"
-    "https://github.com/Fannovel16/comfyui_controlnet_aux"
-    "https://github.com/Gourieff/comfyui-reactor-node"
+    "https://github.com/yolain/ComfyUI-Easy-Use"
+    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    "https://github.com/rgthree/rgthree-comfy"
     "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
     "https://github.com/WASasquatch/was-node-suite-comfyui"
     "https://github.com/adieyal/comfyui-dynamicprompts"
-    "https://github.com/city96/ComfyUI-GGUF"
     "https://github.com/crystian/ComfyUI-Crystools"
     "https://github.com/cubiq/ComfyUI_InstantID"
     "https://github.com/cubiq/ComfyUI_essentials"
-    "https://github.com/hayden-fr/ComfyUI-Model-Manager"
-    "https://github.com/heshengtao/comfyui_LLM_party"
-    "https://github.com/huchenlei/ComfyUI_densediffusion"
-    "https://github.com/huchenlei/ComfyUI_omost"
     "https://github.com/kijai/ComfyUI-KJNodes"
-    "https://github.com/kijai/ComfyUI-SUPIR"
-    "https://github.com/lks-ai/anynode"
     "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
     "https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
-    "https://github.com/mcmonkeyprojects/sd-dynamic-thresholding"
-    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
-    "https://github.com/rgthree/rgthree-comfy"
-    "https://github.com/space-nuko/ComfyUI-OpenPose-Editor"
-    "https://github.com/space-nuko/nui-suite"
-    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
-    "https://github.com/talesofai/comfyui-browser"
-    "https://github.com/yolain/ComfyUI-Easy-Use"
-    "https://github.com/giriss/comfy-image-saver"
 )
+    # "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    # "https://github.com/Acly/comfyui-inpaint-nodes"
+    # "https://github.com/Gourieff/comfyui-reactor-node"
+    # "https://github.com/city96/ComfyUI-GGUF"
+    # "https://github.com/hayden-fr/ComfyUI-Model-Manager"
+    # "https://github.com/heshengtao/comfyui_LLM_party"
+    # "https://github.com/huchenlei/ComfyUI_densediffusion"
+    # "https://github.com/huchenlei/ComfyUI_omost"
+    # "https://github.com/kijai/ComfyUI-SUPIR"
+    # "https://github.com/lks-ai/anynode"
+    # "https://github.com/mcmonkeyprojects/sd-dynamic-thresholding"
+    # "https://github.com/space-nuko/ComfyUI-OpenPose-Editor"
+    # "https://github.com/space-nuko/nui-suite"
+    # "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+    # "https://github.com/talesofai/comfyui-browser"
+    # "https://github.com/giriss/comfy-image-saver"
+    # "https://github.com/11cafe/comfyui-workspace-manager"
 
 # comfyui venv
 source /opt/ai-dock/bin/venv-set.sh comfyui
@@ -51,3 +51,4 @@ done
 
 $COMFYUI_VENV_PYTHON "${COMFYUI_PATH}/custom_nodes/ComfyUI-Manager/cm-cli.py" install ${install_string}
 
+cecho green "::::: Finished installing the nodes :::::"
